df9be9c172b5d64ec3ae470b701dbabce2b1d528
Kevin <kaacuna20@gmail.com>
2024-06-27 22:22:55 -0500

    fixing orthography in templates

--- api-house-finder/app/main.py
+++ api-house-finder/app/main.py
@@ -5,10 +5,10 @@ app = FastAPI(title="House Finder API_REST Service",
     description="Api service from the webapp house finder",
     summary="This is api space where developers can get and post about housing projects in Atlantico-Colombia.",
     version="0.0.1",
-    terms_of_service="http://localhost:5003/api-documentation",
+    terms_of_service="http://localhost/api-documentation",
     contact={
         "name": "kaacuna",
-        "url": "http://kaacunadev.com",
+        "url": "https://kaacunaword.com/",
         "email": "kaacuna20@gmail.com",
     },
     )
